7f0e66f1f7ff39c391f9886c8ba1cee0a5bcd220
Daniel <daniel.wallner@mbit.at>
2016-03-10 23:18:09 +0100

    ReversePolishNotation Initial

--- Todolist/Main.hs
+++ Todolist/Main.hs
@@ -1,21 +1,39 @@
-import System.IO
+import System.Environment
 import System.Directory
+import System.IO
 import Data.List
 
+dispatch :: [(String, [String] -> IO ())]
+dispatch =  [ ("add", add)
+            , ("view", view)
+            , ("remove", remove)
+            ]
+
 main = do
-    handle <- openFile "todo.txt" ReadMode
-    (tempName, tempHandle) <- openTempFile "." "temp"
-    contents <- hGetContents handle
+    (command:args) <- getArgs
+    let (Just action) = lookup command dispatch
+    action args
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
-    putStrLn "These are your TO-DO items:"
     putStr $ unlines numberedTasks
-    putStrLn "Which one do you want to delete?"
-    numberString <- getLine
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    handle <- openFile fileName ReadMode
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    contents <- hGetContents handle
     let number = read numberString
+        todoTasks = lines contents
         newTodoItems = delete (todoTasks !! number) todoTasks
     hPutStr tempHandle $ unlines newTodoItems
     hClose handle
     hClose tempHandle
-    removeFile "todo.txt"
-    renameFile tempName "todo.txt"
+    removeFile fileName
+    renameFile tempName fileName
