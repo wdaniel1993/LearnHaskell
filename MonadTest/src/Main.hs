0f1268808ddff8f6036b82876d72370dea2b9e93
Daniel <daniel.wallner@mbit.at>
2016-03-16 23:41:59 +0100

    Birds and Pole

--- MonadTest/src/Main.hs
+++ MonadTest/src/Main.hs
@@ -1,2 +1,24 @@
 module Main where
-    main = print 1234
+
+    type Birds = Int
+    type Pole = (Birds,Birds)
+
+    landLeft :: Birds -> Pole -> Maybe Pole
+    landLeft n (left,right)
+        | abs ((left + n) - right) < 4 = Just (left + n, right)
+        | otherwise                    = Nothing
+
+    landRight :: Birds -> Pole -> Maybe Pole
+    landRight n (left,right)
+        | abs (left - (right + n)) < 4 = Just (left, right + n)
+        | otherwise                    = Nothing
+
+    routine :: Maybe Pole
+    routine = do
+        start <- return (0,0)
+        first <- landLeft 2 start
+        second <- landRight 2 first
+        Nothing
+        landLeft 1 second
+
+    main = print routine
